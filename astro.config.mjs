62815b4fff6a784ef48b1cfa66dce8ac208d38b1
patricioreguera <reguerapatriciohernan@gmail.com>
2026-02-28 00:34:38 +0100

    fix(astro): install @astrojs/vercel adapter to enable server-rendered api routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,16 @@ import react from '@astrojs/react';
 
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://mareacreativa.com",
+
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react(), sitemap()]
+  integrations: [react(), sitemap()],
+  adapter: vercel()
 });
